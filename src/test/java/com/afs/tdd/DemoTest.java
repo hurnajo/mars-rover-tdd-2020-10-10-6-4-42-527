b5a6c4fceda9b40043689055bac13c62f0f024ba
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-15 18:48:52 +0800

    [TDD][Jose Leo Hurna] Test - Create Test for status (0,0) E command M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -74,4 +74,16 @@ class DemoTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("W",marsRover.getHeading());
     }
+
+    @Test
+    void should_return_x_1_y_0_heading_E_when_execute_given_x_0_y_0_heading_E_and_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"E");
+        //when
+        marsRover.executeCommands("M");
+        //then
+        assertEquals(1,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("E",marsRover.getHeading());
+    }
 }
