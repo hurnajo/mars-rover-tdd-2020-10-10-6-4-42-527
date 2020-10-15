61ee46eee1e012d910ca1314b8abb90b6109d34e
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-15 20:01:56 +0800

    [TDD][Jose Leo Hurna] Refactor - refactor test case name and variables

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -114,13 +114,13 @@ class DemoTest {
     }
 
     @Test
-    void should_return_x_0_y_0_heading_W_when_execute_given_x_0_y_0_heading_W_and_command_M() {
+    void should_return_x_negative_one_y_0_heading_W_when_execute_given_x_0_y_0_heading_W_and_command_M() {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "W");
         //when
-        marsRover.executeCommands("R");
+        marsRover.executeCommands("M");
         //then
-        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
         assertEquals("W", marsRover.getHeading());
     }
