73918cd0d4811fd01fc4f042c30fdb40209c872c
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-15 19:12:24 +0800

    [TDD][Jose Leo Hurna] Test - Create test for status (0,0) E command R

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -8,83 +8,85 @@ class DemoTest {
     @Test
     void should_return_x_0_y_1_heading_N_when_execute_given_x_0_y_0_heading_N_and_command_M() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"N");
+        MarsRover marsRover = new MarsRover(0, 0, "N");
         //when
         marsRover.executeCommands("M");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(1,marsRover.getLocationY());
-        assertEquals("N",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_given_x_0_y_0_heading_N_and_command_L() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"N");
+        MarsRover marsRover = new MarsRover(0, 0, "N");
         //when
         marsRover.executeCommands("L");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(0,marsRover.getLocationY());
-        assertEquals("W",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getHeading());
     }
 
     @Test
     void should_return_x_0_y_0_heading_E_when_execute_given_x_0_y_0_heading_N_and_command_R() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"N");
+        MarsRover marsRover = new MarsRover(0, 0, "N");
         //when
         marsRover.executeCommands("R");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(0,marsRover.getLocationY());
-        assertEquals("E",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getHeading());
     }
 
     @Test
     void should_return_x_0_y_negative_1_heading_S_when_execute_given_x_0_y_0_heading_S_and_command_M() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"S");
+        MarsRover marsRover = new MarsRover(0, 0, "S");
         //when
         marsRover.executeCommands("M");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(-1,marsRover.getLocationY());
-        assertEquals("S",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationY());
+        assertEquals("S", marsRover.getHeading());
     }
+
     @Test
     void should_return_x_0_y_0_heading_E_when_execute_given_x_0_y_0_heading_S_and_command_L() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"S");
+        MarsRover marsRover = new MarsRover(0, 0, "S");
         //when
         marsRover.executeCommands("L");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(0,marsRover.getLocationY());
-        assertEquals("E",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getHeading());
     }
+
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_given_x_0_y_0_heading_S_and_command_R() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"S");
+        MarsRover marsRover = new MarsRover(0, 0, "S");
         //when
         marsRover.executeCommands("R");
         //then
-        assertEquals(0,marsRover.getLocationX());
-        assertEquals(0,marsRover.getLocationY());
-        assertEquals("W",marsRover.getHeading());
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getHeading());
     }
 
     @Test
     void should_return_x_1_y_0_heading_E_when_execute_given_x_0_y_0_heading_E_and_command_M() {
         //given
-        MarsRover marsRover = new MarsRover(0,0,"E");
+        MarsRover marsRover = new MarsRover(0, 0, "E");
         //when
         marsRover.executeCommands("M");
         //then
-        assertEquals(1,marsRover.getLocationX());
-        assertEquals(0,marsRover.getLocationY());
-        assertEquals("E",marsRover.getHeading());
+        assertEquals(1, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getHeading());
     }
 
     @Test
@@ -97,5 +99,17 @@ class DemoTest {
         assertEquals(0, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getHeading());
+    }
+
+    @Test
+    void should_return_x_0_y_0_heading_S_when_execute_given_x_0_y_0_heading_E_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        //when
+        marsRover.executeCommands("R");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getHeading());
     }
 }
