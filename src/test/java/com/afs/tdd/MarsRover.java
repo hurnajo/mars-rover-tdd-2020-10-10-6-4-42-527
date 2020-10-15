d1193543b989452465c77b5bd3d5bea002b11b77
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-15 17:05:40 +0800

    [TDD][Jose Leo Hurna] Test - Create Test for x and y 0 heading N Command M

---
+++ src/test/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,30 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private final int locationX;
+    private final int locationY;
+    private final String heading;
+
+    public MarsRover(int locationX, int locationY, String heading) {
+
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.heading = heading;
+    }
+
+    public void executeCommands(String command) {
+
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getHeading() {
+        return null;
+    }
+}
